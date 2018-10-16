3feebaaa4b12f2a5c5d45b7bae40907817e04816
guillaume pinon <dragonis41@gmail.com>
2018-10-16 17:12:11 +0200

    add librairie color.h

---
+++ color.h
@@ -0,0 +1,50 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+//TEXT EFFECT COLOR
+#define  DEFAULT  	"\x1B[0m"
+#define  HIGHLIGHT 	"\x1B[1m"
+#define  DIM 		"\x1B[2m"
+#define  ITALIC 	"\x1B[3m"
+#define  UNDERLINE 	"\x1B[4m"
+#define  BLINK 		"\x1B[5m"
+#define  REVERSE 	"\x1B[7m"
+#define  HIDDEN 	"\x1B[8m"
+
+//FORGROUND COLOR (TEXT)
+#define  RED 		"\x1B[31m"
+#define  GREEN 		"\x1B[32m"
+#define  YELLOW 	"\x1B[33m"
+#define  BLUE 		"\x1B[34m"
+#define  PURPLE 	"\x1B[35m"
+#define  CYAN 		"\x1B[36m"
+#define  LGREY 		"\x1B[37m"
+#define  DGREY 		"\x1B[90m"
+#define  LRED 		"\x1B[91m"
+#define  LGREEN 	"\x1B[92m"
+#define  LYELLOW 	"\x1B[93m"
+#define  LBLUE 		"\x1B[94m"
+#define  LMAGENTA 	"\x1B[95m"
+#define  LCYAN 		"\x1B[96m"
+#define  WHITE 		"\x1B[97m"
+
+//bACKGROUND COLOR
+#define  BGDEFAULT 	"\x1B[49m"
+#define  BGBLACK 	"\x1B[40m"
+#define  BGRED 		"\x1B[41m"
+#define  BGGREEN 	"\x1B[42m"
+#define  BGYELLOW 	"\x1B[43m"
+#define  BGBLUE 	"\x1B[44m"
+#define  BGMAGENTA 	"\x1B[45m"
+#define  BGCYAN 	"\x1B[46m"
+#define  BGLGREY 	"\x1B[47m"
+#define  BGDGREY 	"\x1B[100m"
+#define  BGLRED 	"\x1B[101m"
+#define  BGLGREEN 	"\x1B[102m"
+#define  BGLYELLOW 	"\x1B[103m"
+#define  BGLBLUE 	"\x1B[104m"
+#define  BGLMAGENTA 	"\x1B[105m"
+#define  BGLCYAN 	"\x1B[106m"
+
+
+#endif
